9e804120c26564521d541970a702603bf8bd307e
Юрий Агеев <ageev.y@yandex.ru>
2024-04-04 22:38:52 +0300

    trying scraping without redirect.

--- petrovich_parser/spiders/petrovich_spider.py
+++ petrovich_parser/spiders/petrovich_spider.py
@@ -15,50 +15,49 @@ BASE_LINK = 'https://petrovich.ru'
 
 class PetrovichSpiderSpider(scrapy.Spider):
     name = 'petrovich_spider'
-    start_urls = ['https://petrovich.ru/']
+    # start_urls = ['https://petrovich.ru/']
 
-    def start_login(self, response):
+    # def start_login(self, response):
 
-        button = response.css('a.header-button.auth-header-button')
-        button_url = button.css('::attr(href)').extract_first()
-        if button_url:
-            yield response.follow(button_url, callback=self.login)
+    #     button = response.css('a.header-button.auth-header-button')
+    #     button_url = button.css('::attr(href)').extract_first()
+    #     if button_url:
+    #         yield response.follow(button_url, callback=self.login)
 
-    def login(self, response):
+    # def login(self, response):
 
-        sleep(1)
-        email_form_response = response.css('#pt-input-id-5')
-        password_form_response = response.css('#pt-input-id-6')
-        return FormRequest(self.start_urls,
-                           formdata={email_form_response: LOGIN,
-                                     password_form_response: PASSWORD},
-                           callback=self.start_scraping)
+    #     sleep(1)
+    #     email_form_response = response.css('#pt-input-id-5')
+    #     password_form_response = response.css('#pt-input-id-6')
+    #     return FormRequest(self.start_urls,
+    #                        formdata={email_form_response: LOGIN,
+    #                                  password_form_response: PASSWORD},
+    #                        callback=self.start_requests)
 
     def start_requests(self):
 
-        estimates_url = 'https://petrovich.ru/cabinet/estimates/'
-        yield scrapy.Request(estimates_url, callback=self.start_scraping)
+    #     estimates_url = 'https://petrovich.ru/cabinet/estimates/'
+    #     yield scrapy.Request(estimates_url, callback=self.start_scraping, meta={'dont_redirect': True})
 
-        # for url in ESTIMATES_URL:
-        #     yield scrapy.Request(url, callback=self.start_scraping)
+        for url in ESTIMATES_URL:
+            yield scrapy.Request(url, callback=self.parse_estimate, meta={'dont_redirect': True} )
 
-    def start_scraping(self, response):
+    # def start_scraping(self, response):
 
-        for link in response.css('a.title.pt-wrap').attrib['href']:
-            yield response.follow(
-                link, callback=self.parse_estimate
-            )
+    #     for link in response.css('a.title.pt-wrap::attr(href)'):
+    #         yield response.follow(
+    #             link, callback=self.parse_estimate
+    #         )
 
     def parse_estimate(self, response):
 
         products_title = response.css('span.pt-typography____JqPt[data-test="product-title"]::text').getall()
-        link = response.css('a.sc-eqUAAy::attr(href)').getall()
+        links = response.css('a.sc-eqUAAy::attr(href)').getall()
         products_amount = response.css('input[data-test="product-counter"]::attr(value)').getall()
-        products_link = BASE_LINK + link
 
-        for product_title, product_amount, product_link in zip(products_title, products_amount, products_link):
+        for product_title, product_amount, product_link in zip(products_title, products_amount, links):
             yield PetrovichParserItem(
                 name=product_title,
                 amount=product_amount,
-                link=product_link,
+                link=BASE_LINK+product_link,
             )
